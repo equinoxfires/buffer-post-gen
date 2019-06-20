f034c6b69d10a34e0d4bcf373099efc5a931db8f
Evan <efuller@mosaiclearning.com>
2019-06-19 22:42:31 -0400

    Add background generator and logo image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import Textfit from 'react-textfit';
+import logo from './assets/proseProtips.png';
+import img1 from './assets/1.jpg';
+import img2 from './assets/2.jpg';
+import img3 from './assets/3.jpg';
 import './App.css';
 
 function App() {
+  const backgrounds = [img1, img2, img3];
+
+  const testText = 'Write every day.'
+
+  var background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+
+  const bgStyle = { backgroundImage: 'url(' + background + ')' };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={bgStyle}>
+      <div className="colors-holder">
+        <div className="low-pass" />
+        <div className="hue-pass" />
+        <div className="high-pass" />
+      </div>
+      <Textfit className="body-text" mode="multi">
+        {testText}
+      </Textfit>
+
+      <img src={logo} class="App-logo" alt="logo" />
     </div>
   );
 }
