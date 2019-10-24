26cefba922abcea3e9791f6b43e679e504297a4a
Evan <efuller@mosaiclearning.com>
2019-10-23 22:53:53 -0400

    Change fonts Attempt to get a post to Buffer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const testText =
   'It is most commonly used when mix-blend-mode has been declared on another element. Applying isolation to the element guards that element so that it does not inherit the mix-blend-mode applied to the other elements that might be behind it..';
 
 // Buffer information
-// Access token: 1/b22b4d576e4e3b37602d070cd555b190
+let accessToken = '2/1723705287d8b7f869da74b51aff2f2b6ce8032e61a7494a4273e5ed756ebac388668cd75d730596dfe96e7b826d52a75c16653a9fcfcf57c02b86a83f3894c2';
 // Client ID: 5d0cebbf2131fe43e278863c
 // Client Secret: 11d11679cb6da73c3e6def668b8bd8a3
 // Redirect URI: urn: ietf: wg: oauth: 2.0: oob
@@ -38,13 +38,50 @@ class App extends Component {
     });
   }
   generatePost() {
+    let self = this;
     let App = document.getElementById('App');
-    html2canvas(App).then(function(canvas) {
+    html2canvas(App).then(function (canvas) {
       document.body.appendChild(canvas);
       console.log(c2i);
       const generatedImage = c2i.convertToPNG(canvas, 1080, 1350);
       console.log(generatedImage);
+
+      const requestUrl =
+        'https://api.bufferapp.com/1/profiles.json?access_token=' +
+        accessToken +
+        '';
+      const postUrl =
+        'https://api.bufferapp.com/1/updates/create.json?access_token=' +
+        accessToken +
+        '&text=This%20is%20an%20example%20update&media[link]=http%3A%2F%2Fgoogle.com&media[description]=The%20google%20homepage';
+      self.httpGetAsync(requestUrl, function (response) {
+
+        console.log(response);
+        self.httpPostAsync(postUrl, function (response) {
+
+          console.log(response);
+
+        });
+      });
     });
+  }
+  httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        callback(xmlHttp.responseText);
+    };
+    xmlHttp.open('GET', theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+  }
+  httpPostAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        callback(xmlHttp.responseText);
+    };
+    xmlHttp.open('GET', theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
   }
   generateBackground() {
     console.log('called generateBackground');
